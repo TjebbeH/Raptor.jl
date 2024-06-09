c2ecdb0f9432e1df3f6b3e93e9fc45b4dfd25a2e
Tjebbe Hepkema <t.m.hepkema@uu.n>
2024-06-09 13:13:32 +0200

    update test folders

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test, SafeTestsets
     @time @safetestset "Mcraptor functions" begin
         include("./raptor_algorithm/raptor_functions/test_pareto.jl")
     end
-    # @time @safetestset "Timetable functions" begin
-    #     include("./raptor_timetable/test_timetable_functions.jl")
-    # end
+    @time @safetestset "Timetable functions" begin
+        include("./raptor_timetable/test_timetable_functions.jl")
+    end
 end
