de4b7af8db37a9bbfb022d3f4b58d1311624ac17
Tjebbe Hepkema <t.m.hepkema@uu.n>
2024-06-10 20:23:21 +0200

    change logger for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,14 @@
 using Test, SafeTestsets
+import Logging: Warn, ConsoleLogger, with_logger
 
-
-@safetestset "Mcraptor functions" begin
-    include("./raptor_algorithm/raptor_functions/test_pareto.jl")
-end
-@safetestset "Timetable creation" begin
-    include("./raptor_timetable/test_timetable_creation.jl")
-end
-@safetestset "Gtfs Timetable" begin
-    include("./gtfs/test_gtfs_parse.jl")
+with_logger(ConsoleLogger(stderr, Warn)) do
+    @safetestset "Mcraptor functions" begin
+        include("./raptor_algorithm/raptor_functions/test_pareto.jl")
+    end
+    @safetestset "Timetable creation" begin
+        include("./raptor_timetable/test_timetable_creation.jl")
+    end
+    @safetestset "Gtfs Timetable" begin
+        include("./gtfs/test_gtfs_parse.jl")
+    end
 end
