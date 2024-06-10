a9b748719171801b8dd8cfb40ac88cf3e928ee97
Tjebbe Hepkema <t.m.hepkema@uu.n>
2024-06-10 19:28:31 +0200

    tests for timetable creation

---
+++ test/raptor_timetable/test_timetable_creation.jl
@@ -0,0 +1,77 @@
+import Raptor: parse_gtfs
+import Raptor: create_stops, Stop
+import Raptor: create_stations, stops_with_stopname, Station
+import Raptor: create_trips, Trip, StopTime, Route
+
+using Dates
+using DataFrames
+
+date = Date(2021,10,21)
+path = joinpath([@__DIR__,"..","gtfs","testdata","gtfs_test"])
+gtfs_timetable = parse_gtfs(path, date);
+
+# Test if correct stops are created
+stops = create_stops(gtfs_timetable.stops)
+expected_stops = Dict(
+    "2473089" => Stop("2473089", "Station A", "2"),
+    "2473090" => Stop("2473090", "Station B", "?"),
+)
+@test stops == expected_stops
+
+# Test if stops_with_stopname selects correct rows from dataframe
+stops_at_station_A = stops_with_stopname("Station A", gtfs_timetable.stops)
+expected_stops_at_station_A = DataFrame(
+    stop_id=["2473089"],
+    stop_name = ["Station A"], 
+    stop_code=["STA"],
+    platform_code=["2"]
+)
+@test stops_at_station_A == expected_stops_at_station_A
+
+# Test if correct stations are created
+stations = create_stations(gtfs_timetable.stops)
+expected_stations = Dict(
+    "STA" => Station("STA", "Station A", [Stop("2473089", "Station A", "2")]),
+    "STB" => Station("STB", "Station B", [Stop("2473090", "Station B", "?")]),
+)
+@test stations == expected_stations
+
+# Test if correct trips are created
+trips = create_trips(gtfs_timetable.trips, gtfs_timetable.stop_times, stops)
+expected_route = Route(
+    "67394",
+    [Stop("2473089", "Station A", "2"), Stop("2473090", "Station B", "?")]
+)
+expected_stop_times_1 = [
+    StopTime(
+        Stop("2473089", "Station A", "2"),
+        DateTime(2021,10,21,11,58,0),
+        DateTime(2021,10,21,12,0,0),
+        0,
+    ),
+    StopTime(
+        Stop("2473090", "Station B", "?"),
+        DateTime(2021,10,21,14,0,0),
+        DateTime(2021,10,21,14,0,0),
+        0,
+    ),
+]
+expected_stop_times_2 = [
+    StopTime(
+        Stop("2473089", "Station A", "2"),
+        DateTime(2021,10,21,23,50,0),
+        DateTime(2021,10,21,23,51,0),
+        0,
+    ),
+    StopTime(
+        Stop("2473090", "Station B", "?"),
+        DateTime(2021,10,22,0,30,0),
+        DateTime(2021,10,22,0,32,0),
+        0,
+    ),
+]
+expected_trips = Dict(
+    "191659463" => Trip("191659463", "525", "Intercity", expected_route, expected_stop_times_1),
+    "191659464" => Trip("191659464", "527", "Intercity", expected_route, expected_stop_times_2)
+)
+@test trips == expected_trips
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using Test, SafeTestsets
 @time @safetestset "Mcraptor functions" begin
     include("./raptor_algorithm/raptor_functions/test_pareto.jl")
 end
-@time @safetestset "Timetable functions" begin
-    include("./raptor_timetable/test_timetable_functions.jl")
+@time @safetestset "Timetable creation" begin
+    include("./raptor_timetable/test_timetable_creation.jl")
 end
 @time @safetestset "Gtfs Timetable" begin
     include("./gtfs/test_gtfs_parse.jl")
