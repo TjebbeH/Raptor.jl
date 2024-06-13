e7a59c8424fbd7e326f39367d413ef1219dd64e3
Tjebbe Hepkema <t.m.hepkema@uu.n>
2024-06-13 22:18:24 +0200

    start of recursively construct journey in example

--- examples/example.jl
+++ examples/example.jl
@@ -1,7 +1,6 @@
 using Revise  
 
 using Raptor
-import Raptor: get_station, StationAbbreviation
 using Dates
 
 
@@ -16,13 +15,13 @@ save_timetable(timetable)
 # date = Date(2024, 5, 20)
 # timetable = load_timetable();
 
-origin = "ASN";
-destination = "GN"
+origin = "UT"
+destination = "DV"
 departure_time = DateTime(2024, 5, 20, 12, 0, 0);
 
 query = McRaptorQuery(origin, destination, departure_time, timetable);
 
-maximum_rounds = 2
+maximum_rounds = 3
 
 # import Logging: Debug, ConsoleLogger, with_logger
 # with_logger(ConsoleLogger(stderr, Debug)) do
@@ -30,32 +29,27 @@ maximum_rounds = 2
 # end;
 
 bag_round_stop, last_round = run_mc_raptor(timetable, query, maximum_rounds);
-bags_to_destination = [bag_round_stop[last_round][s] for s in destination.stops];
-for bag in bags_to_destination
-    for option in bag.options
-        @show option.label
-        @show option.from_stop
-        @show option.trip.name
-    end
-end
+# bags_to_destination = [bag_round_stop[last_round][s] for s in query.destination.stops];
+# for bag in bags_to_destination
+#     for option in bag.options
+#         @show option.label
+#         @show option.from_stop
+#         @show option.trip.name
+#     end
+# end
 
 
 
-import Raptor:merge_bags, Stop, Option, Trip,get_stop_time
+import Raptor:merge_bags, Stop, Option, Trip,get_stop_time, get_station
 bag_last_round = bag_round_stop[last_round]
 
-destination_station = timetable.stations["GN"]
-to_stops = destination_station.stops
+to_stops = query.destination.stops
 station_bag = merge_bags([bag_last_round[s] for s in to_stops])
 to_stop = keys(filter(x -> x[2] == station_bag, bag_last_round)) |> only 
 
 include("../src/utils.jl")
 include("../src/raptor_algorithm/journey_structs.jl")
-
-option = station_bag.options[1]
-label = option.label
-trip = option.trip
-
+include("../src/raptor_algorithm/construct_journeys.jl")
 
 legs = JourneyLegs(station_bag.options, to_stop)
 if isempty(legs)
@@ -63,4 +57,20 @@ if isempty(legs)
 end
 
 journeys = [Journey([leg]) for leg in legs]
+
+journey = journeys[2]
+first_leg = journey.legs[1]
+display_leg(first_leg)
+# if first_leg.from_stop in query.origin.stops
+    # display_journey(journey)
+# else 
+new_to_stops = get_station(first_leg.from_stop, timetable).stops
+station_bag = merge_bags([bag_last_round[s] for s in new_to_stops])
+new_to_stop = keys(filter(x -> x[2] == station_bag, bag_last_round)) |> only 
+new_legs = JourneyLegs(station_bag.options, new_to_stop)
+new_first_leg = new_legs[1]
+display_leg(new_first_leg)
+prepend!(journey.legs, [new_first_leg])
+display_journey(journey)
+
 
--- src/raptor_algorithm/construct_journeys.jl
+++ src/raptor_algorithm/construct_journeys.jl
@@ -1,2 +1,18 @@
 
 is_transfer(leg::JourneyLeg) = leg.from_stop == leg.to_stop
+
+
+function display_journey(journey::Journey)
+    for leg in journey.legs
+        println(display_leg(leg))
+    end
+end
+
+function display_leg(leg::JourneyLeg)
+    from_station = "$(leg.from_stop.station_name) sp.$(leg.from_stop.platform_code)"
+    to_station = "$(leg.to_stop.station_name) sp.$(leg.to_stop.platform_code)"
+    trip = "$(leg.trip.name)"
+    arrival_time = "$(Dates.format(leg.arrival_time, dateformat"HH:MM"))"
+    departure_time = "$(Dates.format(leg.departure_time, dateformat"HH:MM"))"
+    return "$from_station ($departure_time)  to  $to_station ($arrival_time)  with  $trip"
+end
