1a977f64ba4da1024bf38a18d1bcf06d1b8691d9
dafne.vankuppevelt@ns.nl <>
2024-06-21 15:24:42 +0000

    create data dir in setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,3 +6,4 @@ git config --global core.autocrlf input
 git config --global user.email "${GIT_MAIL}"
 git config --global user.name "${GIT_EMAIL}"
 julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
+mkdir -p ./src/raptor_timetable/data
