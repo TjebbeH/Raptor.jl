f882829df7544049c3020bd3904165aaf1d2ea1f
Tjebbe Hepkema <t.m.hepkema@uu.n>
2024-06-22 18:54:37 +0200

    format

--- examples/all_journey_options.jl
+++ examples/all_journey_options.jl
@@ -8,13 +8,13 @@ using Dates
 
 import Raptor: create_raptor_timetable
 import Raptor: save_timetable
-gtfs_dir = joinpath([@__DIR__, "..", "src","gtfs","data","gtfs_nl_2024_05_20"])
-date = Date(2024,5,20)
-timetable = create_raptor_timetable(gtfs_dir,date);
+gtfs_dir = joinpath([@__DIR__, "..", "src", "gtfs", "data", "gtfs_nl_2024_05_20"])
+date = Date(2024, 5, 20)
+timetable = create_raptor_timetable(gtfs_dir, date);
 save_timetable(timetable)
 
 import Raptor: load_timetable
-date = Date(2024,5,20)
+date = Date(2024, 5, 20)
 # date = Date(2024, 6, 19)
 timetable = load_timetable();
 maximum_number_of_transfers = 5
