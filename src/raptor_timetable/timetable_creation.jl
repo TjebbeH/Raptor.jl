d94106049657985122cbb1431c0de37b9d79b8e6
Tjebbe Hepkema <t.m.hepkema@uu.n>
2024-07-07 21:22:06 +0200

    timetable creation

--- src/raptor_timetable/timetable_creation.jl
+++ src/raptor_timetable/timetable_creation.jl
@@ -1,5 +1,5 @@
+"""Create dict of stops from gtfs stops dataframe"""
 function create_stops(gtfs_stops::DataFrame)
-    """Create dict of stops from gtfs stops dataframe"""
     stops = Stop.(
         String.(gtfs_stops.stop_id),
         gtfs_stops.stop_name,
@@ -8,13 +8,13 @@ function create_stops(gtfs_stops::DataFrame)
     return Dict(stop.id => stop for stop in stops)
 end
 
+"""Get gtfs stops with a certain name"""
 function stops_with_stopname(stop_name::String, gtfs_stops::DataFrame)
-    """Get gtfs stops with a certain name"""
     return filter(:stop_name => ==(stop_name), gtfs_stops)
 end
 
+"""Create station from a row in the gtfs dataframe and gtfs stops"""
 function create_station(stationrow::DataFrameRow, gtfs_stops::DataFrame)
-    """Create station from a row in the gtfs dataframe and gtfs stops"""
     name = stationrow.stop_name
     abbreviation = stationrow.stop_code
 
@@ -24,8 +24,8 @@ function create_station(stationrow::DataFrameRow, gtfs_stops::DataFrame)
     return Station(abbreviation, name, stops)
 end
 
+"""Create dict of stations from gtfs stops"""
 function create_stations(gtfs_stops::DataFrame)
-    """Create dict of stations from gtfs stops"""
     gtfs_stations = select(gtfs_stops, [:stop_name, :stop_code]) |> unique
     return Dict(
         stationrow.stop_code => create_station(stationrow, gtfs_stops)
@@ -34,17 +34,17 @@ function create_stations(gtfs_stops::DataFrame)
     )
 end
 
+"""Get stoptimes with a certain trip_id in gtfs stoptimes dataframe"""
 function stop_times_with_trip_id(trip_id::String, gtfs_stop_times::DataFrame)
-    """Get stoptimes with a certain trip_id in gtfs stoptimes dataframe"""
     return filter(:trip_id => ==(trip_id), gtfs_stop_times)
 end
 
+"""Create a trip from a gtfs dataframes and a dict of stops"""
 function create_trip(
         triprow::DataFrameRow,
         gtfs_stop_times::DataFrame,
         stops::Dict{String, Stop}
 )
-    """Create a trip from a gtfs dataframes and a dict of stops"""
     trip_id = triprow.trip_id
     trip_name = triprow.trip_short_name
     trip_formula = triprow.trip_long_name
@@ -63,12 +63,12 @@ function create_trip(
     return Trip(string(trip_id), trip_name, trip_formula, route, stop_times)
 end
 
+"""Create trips from gtfs trips, stoptimes and a dict of already parsed stops"""
 function create_trips(
         gtfs_trips::DataFrame,
         gtfs_stop_times::DataFrame,
         stops::Dict{String, Stop}
 )
-    """Create trips from gtfs trips, stoptimes and a dict of already parsed stops"""
     return Dict(
         triprow.trip_id => create_trip(triprow, gtfs_stop_times, stops)
     for
@@ -76,8 +76,8 @@ function create_trips(
     )
 end
 
+"""Create a dict with footpaths for every combination of stops in a station"""
 function create_footpaths(stations::Dict{String, Station}, duration_sec::Number)
-    """Create a dict with footpaths for every combination of stops in a station"""
     footpaths = Dict()
     for station in values(stations)
         stops = station.stops
@@ -97,34 +97,34 @@ function period(trips::Dict{String, Trip})
     (first_arrival = first_arrival_time(trips), last_departure = last_departure_time(trips))
 end
 
+"""Get all indices of a vector of routes that serve stop"""
 function get_route_idx_serving_stop(stop::Stop, routes::Vector{Route})
-    """Get all indices of a vector of routes that serve stop"""
     return findall(route -> stop in route.stops, routes)
 end
 
+"""Get the stop index in a vector of stops"""
 function get_stop_idx(stop::Stop, stops::Vector{Stop})
-    """Get the stop index in a vector of stops"""
     return findall(s -> s == stop, stops) |> only
 end
 
+"""Get dict of route indices and the stop index of the stop on the route"""
 function get_routes_and_stop_idx(stop::Stop, routes::Vector{Route})
-    """Get dict of route indices and the stop index of the stop on the route"""
     route_idx = get_route_idx_serving_stop(stop, routes)
     return Dict(routes[idx] => get_stop_idx(stop, routes[idx].stops) for idx in route_idx)
 end
 
+"""Create lookup dict to search routes that serve a given stop"""
 function create_stop_routes_lookup(stops::Vector{Stop}, routes::Vector{Route})
-    """Create lookup dict to search routes that serve a given stop"""
     return Dict(stop => get_routes_and_stop_idx(stop, routes) for stop in stops)
 end
 
+"""Get all trips that travel along a route"""
 function get_trips_of_route(trips::Vector{Trip}, route::Route)
-    """Get all trips that travel along a route"""
     return filter(t -> t.route == route, trips)
 end
 
+"""Create a lookup dict to search all routes that travel along a route"""
 function create_route_trip_lookup(trips::Vector{Trip}, routes::Vector{Route})
-    """Create a lookup dict to search all routes that travel along a route"""
     return Dict(route => get_trips_of_route(trips, route) for route in routes)
 end
 
