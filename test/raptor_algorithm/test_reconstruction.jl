cc08382db63a8095b5ed91e83be16fa2a88f9f95
Tjebbe Hepkema <t.m.hepkema@uu.n>
2024-09-27 15:30:51 +0200

    first tests for reconstruction

---
+++ test/raptor_algorithm/test_reconstruction.jl
@@ -0,0 +1,33 @@
+using Raptor
+using Dates
+using Logging
+using Test
+
+import Raptor: last_legs
+
+include("../create_test_timetable.jl")
+timetable = create_test_timetable();
+today = Date(2021, 10, 21)
+
+origin = "S2"
+departure_time = today + Time(13, 15);
+
+query = McRaptorQuery(origin, departure_time, timetable);
+
+bag_round_stop, last_round = run_mc_raptor(timetable, query);
+bag_last_round = bag_round_stop[last_round];
+
+# Test if last legs are correctly reconstructed
+destination = timetable.stations["S4"];
+journeys_with_last_legs = last_legs(destination, bag_last_round);
+@test length(journeys_with_last_legs) == 3
+
+# Again with only 2 rounds (0 transfers)
+query = McRaptorQuery(origin, departure_time, timetable, 2);
+bag_round_stop, last_round = run_mc_raptor(timetable, query);
+bag_last_round = bag_round_stop[last_round];
+
+destination = timetable.stations["S4"];
+journeys_with_last_legs = last_legs(destination, bag_last_round);
+@test length(journeys_with_last_legs) == 2
+
