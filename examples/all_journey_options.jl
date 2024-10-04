7b39a24f44c0fd14485245f67b6c71d74362b158
tjebbe.hepkema@ns.nl <>
2024-10-04 15:19:52 +0000

    update examples

--- examples/all_journey_options.jl
+++ examples/all_journey_options.jl
@@ -2,17 +2,29 @@ using Distributed
 addprocs(4)
 @show nworkers()
 
-@everywhere using Raptor
 
-using Dates
-
-gtfs_dir = joinpath([@__DIR__, "..", "data", "gtfs", "gtfs_nl_2024_07_01"])
-date = Date(2024, 7, 1)
-timetable = create_raptor_timetable(gtfs_dir, date);
-save_timetable(timetable)
-
+# using Raptor
+# gtfs_dir = joinpath([@__DIR__, "..", "data", "gtfs", "gtfs_nl_2024_07_01"])
 # date = Date(2024, 7, 1)
-# timetable = load_timetable();
+# timetable = create_raptor_timetable(gtfs_dir, date);
+# save_timetable(timetable)
 
-maximum_transfers = 5
+@everywhere begin
+    using Raptor
+    using Dates
+    using Parquet2
+    using DataFrames
+    
+    date = Date(2024, 7, 1)
+    timetable = load_timetable();
+    maximum_transfers = 1;
+    
+    # using Logging
+    # warn_logger = ConsoleLogger(stderr, Logging.Warn)
+    # global_logger(warn_logger)
+end
+
+# using BenchmarkTools
 journeys = @time calculate_all_journeys(timetable, date, maximum_transfers);
+
+Parquet2.writefile("output/journey_legs.parquet", journeys)
--- examples/range_mc_raptor_example.jl
+++ examples/range_mc_raptor_example.jl
@@ -1,26 +1,40 @@
+using Revise
 using Raptor
 
 using Dates
+import Raptor: run_mc_raptor_and_construct_journeys2, journey_dataframe
 
-gtfs_dir = joinpath([@__DIR__, "..", "data", "gtfs", "gtfs_nl_2024_07_01"])
-date = Date(2024, 7, 1)
-timetable = create_raptor_timetable(gtfs_dir, date);
-save_timetable(timetable)
-
+# gtfs_dir = joinpath([@__DIR__, "..", "data", "gtfs", "gtfs_nl_2024_07_01"])
 # date = Date(2024, 7, 1)
-# timetable = load_timetable();
+# timetable = create_raptor_timetable(gtfs_dir, date);
+# save_timetable(timetable)
+
+date = Date(2024, 7, 1)
+timetable = load_timetable();
 
 origin = "VS"
 departure_time_min = date + Time(9)
 departure_time_max = date + Time(15)
 
 range_query = RangeMcRaptorQuery(origin, departure_time_min, departure_time_max, timetable);
-journeys = run_mc_raptor_and_construct_journeys(timetable, range_query);
+using BenchmarkTools
+journeys = @btime run_mc_raptor_and_construct_journeys(timetable, range_query);
+journeys2 = @btime run_mc_raptor_and_construct_journeys2(timetable, range_query);
 
-destination = "AKM"
-destination_station = try_to_get_station(destination, timetable);
-println(journeys[destination_station])
+
+df = @btime journey_leg_dataframe(journeys2);
+df2 = journey_dataframe(journeys2);
+first(df,10)
+df2
+
+# destination = "AKM"
+# destination_station = try_to_get_station(destination, timetable);
+# println(journeys[destination_station])
 
 destination = "GN"
 destination_station = try_to_get_station(destination, timetable);
-println(journeys[destination_station])
+# println(journeys[destination_station])
+
+using BenchmarkTools
+
+journey_leg_dataframe(journeys[destination_station])
