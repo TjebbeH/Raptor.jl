aca85998298a7991ac146a49d0f9d16593493499
tjebbe.hepkema@ns.nl <>
2025-02-21 20:47:15 +0000

    fix typo

--- examples/range_mc_raptor_example.jl
+++ examples/range_mc_raptor_example.jl
@@ -16,8 +16,7 @@ departure_time_max = date + Time(23,59)
 
 range_query = RangeMcRaptorQuery(origin, departure_time_min, departure_time_max, timetable);
 
-# journeys = @btime run_mc_raptor_and_construct_journeys(timetable, range_query);
-journeys2 = @btime run_mc_raptor_and_construct_journeys2(timetable, range_query);
+journeys = @btime run_mc_raptor_and_construct_journeys(timetable, range_query);
 
 df = journey_leg_dataframe(journeys2);
 df2 = journey_dataframe(journeys2);
@@ -29,5 +28,5 @@ destination_station = get_station(destination, timetable);
 println(journeys[destination_station])
 
 destination = "GN"
-destination_station = to_get_station(destination, timetable);
+destination_station = get_station(destination, timetable);
 println(journeys[destination_station])
