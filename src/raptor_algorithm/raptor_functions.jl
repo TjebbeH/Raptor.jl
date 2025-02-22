c3157e8ecf3d23fc9af49c5140cd7a7680849edf
TjebbeH <tjebbe.hepkema@gmail.com>
2025-02-22 11:51:29 +0100

    fix/full_day

--- src/raptor_algorithm/raptor_functions.jl
+++ src/raptor_algorithm/raptor_functions.jl
@@ -387,8 +387,8 @@ function calculate_all_journeys(
     stations = sort(collect(values(timetable.stations)); by=station -> station.name)
 
     all_journeys = @sync @distributed (merge!) for origin in stations
-        departure_time_min = date + Time(8)
-        departure_time_max = date + Time(8, 59)
+        departure_time_min = date + Time(0)
+        departure_time_max = date + Time(23, 59)
 
         range_query = RangeMcRaptorQuery(
             origin, departure_time_min, departure_time_max, maximum_transfers
