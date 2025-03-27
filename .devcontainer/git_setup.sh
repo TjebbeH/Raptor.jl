455cf1185d78d38258c1ebe797bb657b517c823c
Tjebbe Hepkema <tjebbe.hepkema@ns.nl>
2025-03-27 16:17:53 +0000

    fix bug in docs

--- .devcontainer/git_setup.sh
+++ .devcontainer/git_setup.sh
@@ -3,5 +3,6 @@
 git config --global credential.https://dev.azure.com.useHttpPath true
 git config --global --add --bool push.autoSetupRemote true # dont need to do git push --set-upstream origin feature/branch anymore
 git config --global core.autocrlf input
-git config --global user.email "${GIT_MAILLL}"
-git config --global user.name "${GIT_EMAIL}"
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global --add safe.directory /workspaces/Raptor.jl
